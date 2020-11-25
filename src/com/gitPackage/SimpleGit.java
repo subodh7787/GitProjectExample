28e52ae18ea13443d4edf930f7500bb5c0845c19
subodh <subodh@LAPTOP-LMS3206I>
2020-11-25 13:37:57 +0530

    First commit.........

---
+++ src/com/gitPackage/SimpleGit.java
@@ -0,0 +1,15 @@
+package com.gitPackage;
+
+public class SimpleGit {
+
+	public static void main(String args[])
+	{
+		String name="Subodh Kumar";
+		
+		if(name=="Subodh Kumar")
+			System.out.println("Logged In!!!!!!!!!!!!");
+		else
+			System.out.println("NOt Logged In!!!!!!!!!!!");
+		
+	}
+}
